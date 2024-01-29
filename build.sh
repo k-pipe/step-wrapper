1fd3145cca73b7b0be64dfb65562379f43979958
Jan Kneissler <jan.kneissler@breuninger.de>
2024-01-29 12:26:50 +0100

    test exit codes in build script

--- build.sh
+++ build.sh
@@ -6,12 +6,24 @@ echo "===================================================================="
 echo "  Building docker image $IMAGE"
 echo "===================================================================="
 docker build . -t $IMAGE
+if [ $? -ne 0 ]
+then
+   exit 1
+fi
 echo "===================================================================="
 echo "  Logging in to dockerhub with user $USER"
 echo "===================================================================="
 docker login --username $USER --password $DOCKERHUB_PUSH_TOKEN
+if [ $? -ne 0 ]
+then
+   exit 1
+fi
 echo "===================================================================="
 echo "  Pushing image $IMAGE_ID"
 echo "===================================================================="
 docker push $IMAGE
+if [ $? -ne 0 ]
+then
+   exit 1
+fi
 echo "Done"
