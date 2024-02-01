c2491f4948192ee27fa7939b10369c27dbf6a891
Jan Kneissler <jan.kneissler@breuninger.de>
2024-02-01 15:20:34 +0100

    add commands FOR_FILES and WITH_LINES

--- src/org/kpipe/CommandExecutor.java
+++ src/org/kpipe/CommandExecutor.java
@@ -1,17 +1,26 @@
 package org.kpipe;
 
+import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.regex.Pattern;
 import java.util.stream.Collectors;
 
 public class CommandExecutor {
 
     public static final String WITH = "WITH:";
     public static final String CALL = "CALL:";
+
+    public static final String FOR_FILES = "FOR_FILES:";
+
+    public static final String FILE = "file";
+
+    public static final String WITH_LINES = "WITH_LINES:";
+
     private static final char COMMENT_CHAR = '#';
 
     public CommandExecutor() {
@@ -25,15 +34,56 @@ public class CommandExecutor {
         if (commands.isEmpty()) {
             return WrapperState.SUCCESS;
         }
-        String first = commands.get(0);
+        String first = resolve(variables, commands.get(0));
+        List<String> rest = commands.subList(1, commands.size());
         if (first.startsWith(WITH)) {
             String path = Constants.WORKDIR_INPUT+"/"+first.substring(WITH.length());
             readVariables(path, variables);
-            return executeRecursively(commands.subList(1, commands.size()), variables);
+            return executeRecursively(rest, variables);
         }
         if (first.startsWith(CALL)) {
             String path = first.substring(CALL.length());
-            return executeRecursively(split(mergeLinesRemovingComments(path)), variables);
+            WrapperState res = executeRecursively(split(mergeLinesRemovingComments(path)), variables);
+            if (res.equals(WrapperState.SUCCESS) && !rest.isEmpty()) {
+                res = executeRecursively(rest, variables);
+            }
+            return res;
+        }
+        if (first.startsWith(FOR_FILES)) {
+            Pattern pattern = Pattern.compile(first.substring(FOR_FILES.length()));
+            WrapperState res = WrapperState.SUCCESS;
+            String[] files = new File(Constants.WORKDIR_INPUT).list((dir, fn) -> pattern.matcher(fn).matches());
+            if (files == null) {
+                Log.error("Files could not be read from folder: "+Constants.WORKDIR_INPUT);
+                return WrapperState.ERROR;
+            }
+            for (String file : files) {
+                variables.put(FILE, file);
+                res = executeRecursively(rest, variables);
+                if (!res.equals(WrapperState.SUCCESS)) {
+                    return res;
+                }
+            }
+            variables.remove(FILE);
+            return res;
+        }
+        if (first.startsWith(WITH_LINES)) {
+            String file = first.substring(WITH_LINES.length());
+            try {
+                WrapperState res = WrapperState.SUCCESS;
+                for (String line : Files.readAllLines(Path.of(Constants.WORKDIR_INPUT, file))) {
+                    LinkedHashMap<String, String> variablesClone = new LinkedHashMap<>(variables);
+                    variablesClone.putAll(Json.asMap(line));
+                    res = executeRecursively(rest, variablesClone);
+                    if (!res.equals(WrapperState.SUCCESS)) {
+                        return res;
+                    }
+                }
+                return res;
+            } catch (IOException e) {
+                Log.exception(e);
+                return WrapperState.ERROR;
+            }
         }
         return new ProcessExecutor(resolve(variables, commands)).execute();
     }
